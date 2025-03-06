6b8624b509b146cefaa69ddd05b2e2c85ca623d8
martryx <martin.itisme@gmail.com>
2025-03-06 12:17:34 +0100

    fix the fucking go code

--- echo/handler.go
+++ echo/handler.go
@@ -2,11 +2,18 @@ package function
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 // Handle a serverless request
 //
 //goland:noinspection GoUnusedExportedFunction
-func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, GitHub Actions v19. You said: %s", string(req))
+func Handle(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read request body", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Hello, GitHub Actions v19. You said: %s", string(reqBody))
 }
